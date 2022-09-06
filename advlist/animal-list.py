a9403e6485ae142efda0ae83f127e9de836ea283
Anthonymous20 <areggi@gmail.com>
2022-09-06 00:18:30 -0400

    More lists but advanced lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Mycode Challeng Lab
+    Crete a list of animals and printing off 5 of them
+    """
+    
+def main():
+    # create a list called list1
+    list1 = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+
+    # display list1
+    print(list1[0] + " " + list1[1] + " " + list1[2] + " " + list1[3] + " " + list1[4])
+main()
